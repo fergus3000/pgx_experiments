7bc38983bdf129abf9576afe577a7405897005db
Ian Ferguson <iferguson76@gmail.com>
2022-08-11 13:18:43 +0200

    Updated summary stats: Use double precision values instead of ints Check for NaN

--- agg_summary_stats/readme.md
+++ agg_summary_stats/readme.md
@@ -1,7 +1,9 @@
 # agg_summary_stats
-Trying to create my own aggregate pgx function to obtain the Sum,Min,Max,Count for an aggregate as an array. Project is based on the max_timed example, and is usable but incomplete.
+Tried to create my own aggregate pgx function to obtain the Sum,Min,Max,Count for an aggregate as json. Project is based on the max_timed example
+Creates a summary stats aggregate that handles doubles (f64), and returns Jsonb containing min, max, sum, count.
+NaN values are excluded from the aggregate.
 
-So far, it creates a summary stats aggregate that handles int32s, and returns Jsonb containing min, max, sum, count.
+Todo.. explore unit testing, find an easy way to produce binaries for use in a real server instance.
 
 Usage:
 Build and run the project with:
@@ -15,13 +17,13 @@ CREATE TABLE public.some_timeseries
 (
     event_time timestamp with time zone NOT NULL,
     timeseries_id integer NOT NULL,
-    event_data integer NOT NULL,
+    event_data double precision NOT NULL,
     CONSTRAINT some_timeseries_pkey PRIMARY KEY (timeseries_id, event_time)
 )
 TABLESPACE pg_default;
 
 INSERT INTO public.some_timeseries (timeseries_id, event_time, event_data) 
-SELECT 123,x,(random()*100)::int
+SELECT 123,x,(random()*100)::double precision
 FROM generate_series('2020-01-01'::timestamptz,'2020-01-01'::timestamptz + ((1000000 -1) * interval '1 second'), '1 second'::interval) AS x; 
 ```
 
--- agg_summary_stats/src/lib.rs
+++ agg_summary_stats/src/lib.rs
@@ -4,10 +4,10 @@ pg_module_magic!();
 
 #[derive(Default)] // this gives our struct an implicit default. Other things you could derive, but not needed here
 pub struct IntegerAvgState { // this is our state for aggregation, not necessarily what we will return
-    sum: i32,
-    n: i32,
-    min: i32,
-    max: i32
+    sum: f64,
+    n: i64,
+    min: f64,
+    max: f64
 }
 
 // define implementation for our aggregate
@@ -16,7 +16,7 @@ impl Aggregate for IntegerAvgState
 {
     const NAME: &'static str = "summary_stats_pgx";
     type State = Internal;
-    type Args = i32; // we just have 1 argument, an int32. Typical invocation: SELECT summary_stats_pgx(some_integer_column) FROM some_table;
+    type Args = f64; // we just have 1 argument, an double(f64). Typical invocation: SELECT summary_stats_pgx(some_double_column) FROM some_table;
     type Finalize = pgx::JsonB; // the Finalize declares what we will return from our aggregate function - we're going to return a jsonb
 
 
@@ -27,16 +27,21 @@ impl Aggregate for IntegerAvgState
         _fcinfo: pg_sys::FunctionCallInfo,
     ) -> Self::State 
     {
-        let inner = unsafe { current.get_or_insert_default::<IntegerAvgState>() }; // if its our first row, we get a default state. Default impl is delivered courtesy of #[derive(Default)]
+        let inner = unsafe { current.get_or_insert_default::<IntegerAvgState>() }; // if its our first row, we get a default state. Default impl is delivered courtesy of #[derive(Default)]           
         // implementation starts here...
-        inner.sum += val;
-        inner.n += 1;
-        if (val < inner.min) {
-            inner.min = val;
+
+        if !val.is_nan() { // check for NaN values, we exclude them from our stats
+            inner.sum += val;
+            // better if I knew how to set IntegerAvgState's default min and max to NaN
+            if val < inner.min || inner.n == 0 {
+                inner.min = val;
+            }
+            if val > inner.max || inner.n == 0 {
+                inner.max = val;
+            }
+            inner.n += 1;
         }
-        if (val > inner.max) {
-            inner.max = val;
-        }
+        // return current state and continue
         current
     }
 
@@ -48,7 +53,6 @@ impl Aggregate for IntegerAvgState
     {
         let inner = unsafe { current.get::<Self>().unwrap() };
         // convert to jsonb for return
-        // todo: return sum,min,max,count as an array. Just want to test if this works first :)
         pgx::JsonB(serde_json::json!({
             "sum" : inner.sum,
             "min" : inner.min,
